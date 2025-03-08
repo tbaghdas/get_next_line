4ae5df1aaed69122c13ba368491ea76526b4a4a7
tbaghdas <patkanyan1@gmail.com>
2025-03-08 18:39:39 +0400

    Add files via upload

--- get_next_line.c
+++ get_next_line.c
@@ -3,14 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: btigran <btigran@student.42.fr>            +#+  +:+       +#+        */
+/*   By: tbaghdas <tbaghdas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/02 16:45:33 by tbaghdas          #+#    #+#             */
-/*   Updated: 2025/03/07 18:19:40 by btigran          ###   ########.fr       */
+/*   Updated: 2025/03/08 18:20:37 by tbaghdas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
+#include "get_next_line.h"
 
 void	ft_line_cpy(char *old, char *line, int i, int fd)
 {
@@ -34,7 +34,7 @@ void	ft_mem_shift(char *old, int start, int limit, int i)
 		idx_shift++;
 	}
 	while (start + idx_shift < limit)
-		old[start + idx_shift++] = 0;
+		old[start + idx_shift++] = '\0';
 }
 
 int	ft_avail_line(char *line, char *old, int fd)
@@ -45,9 +45,9 @@ int	ft_avail_line(char *line, char *old, int fd)
 	int	return_value;
 
 	j = 0;
-	i = fd * 10;
-	limit = (fd + 1) * 10;
-	while (old[i] != '\n' && i < limit)
+	i = fd * BUFFER_SIZE;
+	limit = (fd + 1) * BUFFER_SIZE;
+	while (old[i] != '\n' && i < limit && old[i] != '\0')
 		line[j++] = old[i++];
 	if (i < limit)
 	{
@@ -57,9 +57,18 @@ int	ft_avail_line(char *line, char *old, int fd)
 	else if (old[i - 1] == '\n')
 		return_value = -1;
 	else
-		return_value = i - fd * 10;
-	ft_mem_shift(old, fd * 10, limit, i);
+		return_value = i - fd * BUFFER_SIZE;
+	ft_mem_shift(old, fd * BUFFER_SIZE, limit, i);
 	return (return_value);
+}
+
+void	set_zero(char *arr, int size)
+{
+	int	i;
+	
+	i = 0;
+	while (i < size)
+		arr[i++] = '\0';
 }
 
 char	*get_next_line(int fd)
@@ -67,26 +76,34 @@ char	*get_next_line(int fd)
 	//char		buf[10];
 	char		*line;
 	char		*tmp;
-	static char	old[10000];
+	static char	old[1000 * BUFFER_SIZE];
 	int			i;
 	int			shift;
-
-	count = 10;
+	int			count;
+int g, b = 5;
+	count = BUFFER_SIZE;
 	i = 0;
+	if (read(fd, line, 0) == -1)
+		return (NULL);
 	line = (char *)malloc(count * sizeof(char));
 	if (line == NULL)
 		return (NULL);
+	set_zero(old, 1000 * BUFFER_SIZE);
 	shift = ft_avail_line(line, old, fd);
 	if (shift == -1)
 		return (line);
-	while (read(fd, line + count - 10 + shift, 10 - shift))
+	g =  read(fd, line + count - BUFFER_SIZE + shift, BUFFER_SIZE - shift);
+	if (g == -1 || g < BUFFER_SIZE)
+		return (NULL);
+	while (g != -1  && g < BUFFER_SIZE && b !=0)
 	{
-		i = count - 10 + shift;
-		while (line[i] != '\0')
+		//printf("testing : %s ##\n %d %d %d \n", line, count, shift, g);
+		i = count - BUFFER_SIZE + shift;
+		while (line[i] != '\n')
 			i++;
 		if (i != count)
 			break ;
-		count *= 10;
+		count *= BUFFER_SIZE;
 		tmp = (char *)malloc(count * sizeof(char));
 		if (tmp == NULL)
 		{
@@ -95,9 +112,54 @@ char	*get_next_line(int fd)
 		}
 		ft_line_cpy(line, tmp, 0, 0);
 		free(line);
+		
 		line = tmp;
 		shift = 0;
+		g = read(fd, line + count - BUFFER_SIZE + shift, BUFFER_SIZE - shift);
+		b--;
 	}
 	ft_line_cpy(line, old, i, fd);
+	line[i] = '\0';
 	return (line);
 }
+
+
+#include <fcntl.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+//#define BUFFER_SIZE 32
+
+// Prototype for your get_next_line function (adjust this according to your project)
+char *get_next_line(int fd);
+
+int main(int argc, char **argv)
+{
+    int fd;
+    char *line;
+
+    if (argc != 2)
+    {
+        write(1, "Usage: ./a.out <filename>\n", 25);
+        return (1);
+    }
+
+    fd = open(argv[1], O_RDONLY);
+    if (fd == -1)
+    {
+        perror("Error opening file");
+        return (1);
+    }
+
+    // Read and print lines until EOF
+    while ((line = get_next_line(fd)) != NULL)
+    {
+        printf("%s", line);
+        free(line);  // Free the memory allocated by get_next_line
+    }
+
+    close(fd);
+    return (0);
+}
+
--- get_next_line.h
+++ get_next_line.h
@@ -6,11 +6,18 @@
 /*   By: tbaghdas <tbaghdas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/02 16:45:38 by tbaghdas          #+#    #+#             */
-/*   Updated: 2025/03/02 16:45:39 by tbaghdas         ###   ########.fr       */
+/*   Updated: 2025/03/08 16:44:20 by tbaghdas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+# ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
+#define BUFFER_SIZE 32
 
+#include <stdlib.h>
+#include <unistd.h>
 
 char	*get_next_line(int fd);
+
+# endif
